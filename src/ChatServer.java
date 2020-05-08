cc32c3c8d093c09dc578c0f39fb364b9ac79479b
Alwyn1106 <alwynrhysfinch@gmail.com>
2020-05-08 14:42:18 +0100

    Added a synchronised function to EchoClient for reading input

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -8,11 +8,18 @@ public class ChatServer extends Thread {
 
 
     public static void main(String[] args) {
-
         int port = 14001;
+
+        if (args.length == 2) {
+            if (args[0].equals("-csp") && args[1].length() == 5) {
+                port = Integer.parseInt(args[1]);
+            } else {
+                System.out.println("The command line argument for selecting a port value was not entered correctly");
+            }
+        }
+
         EchoServer server = new EchoServer(port);
         server.start();
     }
-
 
 }
