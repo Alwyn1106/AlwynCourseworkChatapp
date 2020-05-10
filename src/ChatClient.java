143ded76b7bf59fc6e4bc1600cb0669f67edd510
Alwyn1106 <alwynrhysfinch@gmail.com>
2020-05-10 14:37:36 +0100

    Updating the ChatClient Args

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,39 +1,44 @@
 // This is a main for the Client programme
 import java.lang.*;
+import java.net.ConnectException;
 
 
 public class ChatClient extends Thread {
+
 
     public static void main(String[] args) {
         int port = 14001;
         String address = "localhost";
 
-        if (args.length == 2) {
-            if (args[0].equals("-ccp") && args[1].length() == 5) {
-                if (isInteger(args[1])) {
-                    port = Integer.parseInt(args[1]);
+
+        if (args.length > 0) {
+            if (args.length == 2) {
+                if (args[0].equals("-ccp") && args[1].length() == 5) {
+                    if (isInteger(args[1])) {
+                        port = Integer.parseInt(args[1]);
+                    } else {
+                        System.out.println("The inputted port value is not a recognised integer, initialising with default port");
+                    }
+                } else if (args[0].equals("-cca") && args[1].length() == 14) {
+                    address = args[1];
                 } else {
-                    System.out.println("The inputted port value is not a recognised integer, initialising with default port");
+                    System.out.println("The arguments inputted were not recognised, initialising with default values");
                 }
-            } else if (args[0].equals("-cca") && args[1].length() == 14) {
-                address = args[1];
             }
-            else{
-                System.out.println("The arguments inputted were not recognised, initialising with default values");
+            else if (args.length == 4) {
+                if (args[0].equals("−cca") && args[2].equals("-ccp")) {
+                    port = Integer.parseInt(args[3]);
+                    address = args[1];
+                } else {
+                    System.out.println("The inputted arguments were not recognised, initialising with default values");
+                }
             }
-        }
-        else if (args.length == 4){
-            if(args[0].equals("−cca") && args[2].equals("-ccp")) {
-                port = Integer.parseInt(args[3]);
-                address = args[1];
-            }
-            else{
-                System.out.println("The inputted arguments were not recognised, initialising with default values");
+            else {
+                System.out.println ("The arguments given have not been recognised. The programme will initiate with default values");
             }
         }
 
-            EchoClient client = new EchoClient(address, port);
-
+        EchoClient client = new EchoClient(address, port);
 
     }
 
@@ -45,7 +50,6 @@ public class ChatClient extends Thread {
             return false;
         }
     }
-
 
 
 }
--- src/EchoServer.java
+++ src/EchoServer.java
@@ -4,7 +4,6 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-
 import java.util.ArrayList;
 
 
@@ -34,7 +33,7 @@ public class EchoServer extends Thread {
                 ServerClientOrganiser sco = new ServerClientOrganiser(client, "client-" + ID);
                 getClientList().add(sco);
                 sco.start();
-                //start();
+
             }
 
         }
